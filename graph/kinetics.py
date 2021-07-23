f32acfa5fddfeb9635ad39e06b6b45042d5a0fc7
sysu19351146 <61243285+sysu19351146@users.noreply.github.com>
2021-07-23 22:15:01 +0800

    Add files via upload

---
+++ graph/kinetics.py
@@ -0,0 +1,90 @@
+import numpy as np
+import sys
+# sys.path.extend(['E:/大二下/人工智能实验/action_recognition/
+sys.path.append(r'E:/大二下/人工智能实验/action_recognition/2s-AGCN-master')
+# from graph import tools
+import networkx as nx
+
+
+#graph.tools.py
+#######################################################################################################################
+def edge2mat(link, num_node):
+    A = np.zeros((num_node, num_node))
+    for i, j in link:
+        A[j, i] = 1
+    return A
+
+
+def normalize_digraph(A):  # 除以每列的和
+    Dl = np.sum(A, 0)
+    h, w = A.shape
+    Dn = np.zeros((w, w))
+    for i in range(w):
+        if Dl[i] > 0:
+            Dn[i, i] = Dl[i] ** (-1)
+    AD = np.dot(A, Dn)
+    return AD
+
+
+def get_spatial_graph(num_node, self_link, inward, outward):
+    I = edge2mat(self_link, num_node)
+    In = normalize_digraph(edge2mat(inward, num_node))
+    Out = normalize_digraph(edge2mat(outward, num_node))
+    A = np.stack((I, In, Out))
+    return A
+#######################################################################################################################
+
+
+# Joint index:
+# {0,  "Nose"}
+# {1,  "Neck"},
+# {2,  "RShoulder"},
+# {3,  "RElbow"},
+# {4,  "RWrist"},
+# {5,  "LShoulder"},
+# {6,  "LElbow"},
+# {7,  "LWrist"},
+# {8,  "RHip"},
+# {9,  "RKnee"},
+# {10, "RAnkle"},
+# {11, "LHip"},
+# {12, "LKnee"},
+# {13, "LAnkle"},
+# {14, "REye"},
+# {15, "LEye"},
+# {16, "REar"},
+# {17, "LEar"},
+
+# Edge format: (origin, neighbor)
+num_node = 17
+self_link = [(i, i) for i in range(num_node)]
+inward = [(10, 8), (8, 6), (9, 7), (7, 5), # arms
+    (15, 13), (13, 11), (16, 14), (14, 12), # legs
+    (11, 5), (12, 6), (11, 12), (5, 6), # torso
+    (5, 0), (6, 0), (1, 0), (2, 0), (3, 1), (4, 2)]
+outward = [(j, i) for (i, j) in inward]
+neighbor = inward + outward
+
+
+class Graph:
+    def __init__(self, labeling_mode='spatial'):
+        self.A = self.get_adjacency_matrix(labeling_mode)
+        self.num_node = num_node
+        self.self_link = self_link
+        self.inward = inward
+        self.outward = outward
+        self.neighbor = neighbor
+
+    def get_adjacency_matrix(self, labeling_mode=None):
+        if labeling_mode is None:
+            return self.A
+        if labeling_mode == 'spatial':
+            A = get_spatial_graph(num_node, self_link, inward, outward)
+        else:
+            raise ValueError()
+        return A
+
+
+if __name__ == '__main__':
+    A = Graph('spatial').get_adjacency_matrix()
+    print('')
