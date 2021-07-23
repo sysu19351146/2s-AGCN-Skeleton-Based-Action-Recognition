ba2c905cdf27bbebc0025642611a809663049225
sysu19351146 <61243285+sysu19351146@users.noreply.github.com>
2021-07-23 22:15:44 +0800

    Add files via upload

---
+++ feeders/feeder.py
@@ -0,0 +1,368 @@
+import numpy as np
+import pickle
+import torch
+from torch.utils.data import Dataset
+import sys
+
+# sys.path.extend(['../'])
+# from feeders import tools
+
+#feeders.tools.py
+#######################################################################################################################
+import random
+
+import numpy as np
+
+
+def downsample(data_numpy, step, random_sample=True):
+    # input: C,T,V,M
+    begin = np.random.randint(step) if random_sample else 0
+    return data_numpy[:, begin::step, :, :]
+
+
+def temporal_slice(data_numpy, step):
+    # input: C,T,V,M
+    C, T, V, M = data_numpy.shape
+    return data_numpy.reshape(C, T / step, step, V, M).transpose(
+        (0, 1, 3, 2, 4)).reshape(C, T / step, V, step * M)
+
+
+def mean_subtractor(data_numpy, mean):
+    # input: C,T,V,M
+    # naive version
+    if mean == 0:
+        return
+    C, T, V, M = data_numpy.shape
+    valid_frame = (data_numpy != 0).sum(axis=3).sum(axis=2).sum(axis=0) > 0
+    begin = valid_frame.argmax()
+    end = len(valid_frame) - valid_frame[::-1].argmax()
+    data_numpy[:, :end, :, :] = data_numpy[:, :end, :, :] - mean
+    return data_numpy
+
+
+def auto_pading(data_numpy, size, random_pad=False):
+    C, T, V, M = data_numpy.shape
+    if T < size:
+        begin = random.randint(0, size - T) if random_pad else 0
+        data_numpy_paded = np.zeros((C, size, V, M))
+        data_numpy_paded[:, begin:begin + T, :, :] = data_numpy
+        return data_numpy_paded
+    else:
+        return data_numpy
+
+
+def random_choose(data_numpy, size, auto_pad=True):
+    # input: C,T,V,M 随机选择其中一段，不是很合理。因为有0
+    C, T, V, M = data_numpy.shape
+    if T == size:
+        return data_numpy
+    elif T < size:
+        if auto_pad:
+            return auto_pading(data_numpy, size, random_pad=True)
+        else:
+            return data_numpy
+    else:
+        begin = random.randint(0, T - size)
+        return data_numpy[:, begin:begin + size, :, :]
+
+
+def random_move(data_numpy,
+                angle_candidate=[-10., -5., 0., 5., 10.],
+                scale_candidate=[0.9, 1.0, 1.1],
+                transform_candidate=[-0.2, -0.1, 0.0, 0.1, 0.2],
+                move_time_candidate=[1]):
+    # input: C,T,V,M
+    C, T, V, M = data_numpy.shape
+    move_time = random.choice(move_time_candidate)
+    node = np.arange(0, T, T * 1.0 / move_time).round().astype(int)
+    node = np.append(node, T)
+    num_node = len(node)
+
+    A = np.random.choice(angle_candidate, num_node)
+    S = np.random.choice(scale_candidate, num_node)
+    T_x = np.random.choice(transform_candidate, num_node)
+    T_y = np.random.choice(transform_candidate, num_node)
+
+    a = np.zeros(T)
+    s = np.zeros(T)
+    t_x = np.zeros(T)
+    t_y = np.zeros(T)
+
+    # linspace
+    for i in range(num_node - 1):
+        a[node[i]:node[i + 1]] = np.linspace(
+            A[i], A[i + 1], node[i + 1] - node[i]) * np.pi / 180
+        s[node[i]:node[i + 1]] = np.linspace(S[i], S[i + 1],
+                                             node[i + 1] - node[i])
+        t_x[node[i]:node[i + 1]] = np.linspace(T_x[i], T_x[i + 1],
+                                               node[i + 1] - node[i])
+        t_y[node[i]:node[i + 1]] = np.linspace(T_y[i], T_y[i + 1],
+                                               node[i + 1] - node[i])
+
+    theta = np.array([[np.cos(a) * s, -np.sin(a) * s],
+                      [np.sin(a) * s, np.cos(a) * s]])  # xuanzhuan juzhen
+
+    # perform transformation
+    for i_frame in range(T):
+        xy = data_numpy[0:2, i_frame, :, :]
+        new_xy = np.dot(theta[:, :, i_frame], xy.reshape(2, -1))
+        new_xy[0] += t_x[i_frame]
+        new_xy[1] += t_y[i_frame]  # pingyi bianhuan
+        data_numpy[0:2, i_frame, :, :] = new_xy.reshape(2, V, M)
+
+    return data_numpy
+
+
+def random_shift(data_numpy):
+    # input: C,T,V,M 偏移其中一段
+    C, T, V, M = data_numpy.shape
+    data_shift = np.zeros(data_numpy.shape)
+    valid_frame = (data_numpy != 0).sum(axis=3).sum(axis=2).sum(axis=0) > 0
+    begin = valid_frame.argmax()
+    end = len(valid_frame) - valid_frame[::-1].argmax()
+
+    size = end - begin
+    bias = random.randint(0, T - size)
+    data_shift[:, bias:bias + size, :, :] = data_numpy[:, begin:end, :, :]
+
+    return data_shift
+
+
+def openpose_match(data_numpy):
+    C, T, V, M = data_numpy.shape
+    assert (C == 3)
+    score = data_numpy[2, :, :, :].sum(axis=1)
+    # the rank of body confidence in each frame (shape: T-1, M)
+    rank = (-score[0:T - 1]).argsort(axis=1).reshape(T - 1, M)
+
+    # data of frame 1
+    xy1 = data_numpy[0:2, 0:T - 1, :, :].reshape(2, T - 1, V, M, 1)
+    # data of frame 2
+    xy2 = data_numpy[0:2, 1:T, :, :].reshape(2, T - 1, V, 1, M)
+    # square of distance between frame 1&2 (shape: T-1, M, M)
+    distance = ((xy2 - xy1) ** 2).sum(axis=2).sum(axis=0)
+
+    # match pose
+    forward_map = np.zeros((T, M), dtype=int) - 1
+    forward_map[0] = range(M)
+    for m in range(M):
+        choose = (rank == m)
+        forward = distance[choose].argmin(axis=1)
+        for t in range(T - 1):
+            distance[t, :, forward[t]] = np.inf
+        forward_map[1:][choose] = forward
+    assert (np.all(forward_map >= 0))
+
+    # string data
+    for t in range(T - 1):
+        forward_map[t + 1] = forward_map[t + 1][forward_map[t]]
+
+    # generate data
+    new_data_numpy = np.zeros(data_numpy.shape)
+    for t in range(T):
+        new_data_numpy[:, t, :, :] = data_numpy[:, t, :, forward_map[
+                                                             t]].transpose(1, 2, 0)
+    data_numpy = new_data_numpy
+
+    # score sort
+    trace_score = data_numpy[2, :, :, :].sum(axis=1).sum(axis=0)
+    rank = (-trace_score).argsort()
+    data_numpy = data_numpy[:, :, :, rank]
+
+    return data_numpy
+#######################################################################################################################
+
+
+class Feeder(Dataset):
+    def __init__(self, data_path, label_path,
+                 random_choose=False, random_shift=False, random_move=False,
+                 window_size=-1, normalization=False, debug=False, use_mmap=True):
+        """
+        
+        :param data_path: 
+        :param label_path: 
+        :param random_choose: If true, randomly choose a portion of the input sequence
+        :param random_shift: If true, randomly pad zeros at the begining or end of sequence
+        :param random_move: 
+        :param window_size: The length of the output sequence
+        :param normalization: If true, normalize input sequence
+        :param debug: If true, only use the first 100 samples
+        :param use_mmap: If true, use mmap mode to load data, which can save the running memory
+        """
+
+        self.debug = debug
+        self.data_path = data_path
+        self.label_path = label_path
+        self.random_choose = random_choose
+        self.random_shift = random_shift
+        self.random_move = random_move
+        self.window_size = window_size
+        self.normalization = normalization
+        self.use_mmap = use_mmap
+        self.load_data()
+        if normalization:
+            self.get_mean_map()
+
+    def load_data(self):
+        # data: N C V T M
+
+        try:
+            with open(self.label_path) as f:
+                self.sample_name, self.label = pickle.load(f)
+        except:
+            # for pickle file from python2
+            with open(self.label_path, 'rb') as f:
+                self.sample_name, self.label = pickle.load(f, encoding='latin1')
+
+        # load data
+        if self.use_mmap:
+            self.data = np.load(self.data_path, mmap_mode='r')
+        else:
+            self.data = np.load(self.data_path)
+        if self.debug:
+            self.label = self.label[0:100]
+            self.data = self.data[0:100]
+            self.sample_name = self.sample_name[0:100]
+
+    def get_mean_map(self):
+        data = self.data
+        N, C, T, V, M = data.shape
+        self.mean_map = data.mean(axis=2, keepdims=True).mean(axis=4, keepdims=True).mean(axis=0)
+        self.std_map = data.transpose((0, 2, 4, 1, 3)).reshape((N * T * M, C * V)).std(axis=0).reshape((C, 1, V, 1))
+
+    def __len__(self):
+        return len(self.label)
+
+    def __iter__(self):
+        return self
+
+    def __getitem__(self, index):
+        data_numpy = self.data[index]
+        label = self.label[index]
+        data_numpy = np.array(data_numpy)
+
+        if self.normalization:
+            data_numpy = (data_numpy - self.mean_map) / self.std_map
+        if self.random_shift:
+            # data_numpy = tools.random_shift(data_numpy)
+            data_numpy = random_shift(data_numpy)
+        if self.random_choose:
+            # data_numpy = tools.random_choose(data_numpy, self.window_size)
+            data_numpy = random_choose(data_numpy, self.window_size)
+        elif self.window_size > 0:
+            # data_numpy = tools.auto_pading(data_numpy, self.window_size)
+            data_numpy = auto_pading(data_numpy, self.window_size)
+        if self.random_move:
+            # data_numpy = tools.random_move(data_numpy)
+            data_numpy = random_move(data_numpy)
+
+        return data_numpy, label, index
+
+    def top_k(self, score, top_k):
+        rank = score.argsort()
+        hit_top_k = [l in rank[i, -top_k:] for i, l in enumerate(self.label)]
+        return sum(hit_top_k) * 1.0 / len(hit_top_k)
+
+
+def import_class(name):
+    components = name.split('.')
+    mod = __import__(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+
+def test(data_path, label_path, vid=None, graph=None, is_3d=False):
+    '''
+    vis the samples using matplotlib
+    :param data_path: 
+    :param label_path: 
+    :param vid: the id of sample
+    :param graph: 
+    :param is_3d: when vis NTU, set it True
+    :return: 
+    '''
+    import matplotlib.pyplot as plt
+    loader = torch.utils.data.DataLoader(
+        dataset=Feeder(data_path, label_path),
+        batch_size=64,
+        shuffle=False,
+        num_workers=2)
+
+    if vid is not None:
+        sample_name = loader.dataset.sample_name
+        sample_id = [name.split('.')[0] for name in sample_name]
+        index = sample_id.index(vid)
+        data, label, index = loader.dataset[index]
+        data = data.reshape((1,) + data.shape)
+
+        # for batch_idx, (data, label) in enumerate(loader):
+        N, C, T, V, M = data.shape
+
+        plt.ion()
+        fig = plt.figure()
+        if is_3d:
+            from mpl_toolkits.mplot3d import Axes3D
+            ax = fig.add_subplot(111, projection='3d')
+        else:
+            ax = fig.add_subplot(111)
+
+        if graph is None:
+            p_type = ['b.', 'g.', 'r.', 'c.', 'm.', 'y.', 'k.', 'k.', 'k.', 'k.']
+            pose = [
+                ax.plot(np.zeros(V), np.zeros(V), p_type[m])[0] for m in range(M)
+            ]
+            ax.axis([-1, 1, -1, 1])
+            for t in range(T):
+                for m in range(M):
+                    pose[m].set_xdata(data[0, 0, t, :, m])
+                    pose[m].set_ydata(data[0, 1, t, :, m])
+                fig.canvas.draw()
+                plt.pause(0.001)
+        else:
+            p_type = ['b-', 'g-', 'r-', 'c-', 'm-', 'y-', 'k-', 'k-', 'k-', 'k-']
+            import sys
+            from os import path
+            sys.path.append(
+                path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+            G = import_class(graph)()
+            edge = G.inward
+            pose = []
+            for m in range(M):
+                a = []
+                for i in range(len(edge)):
+                    if is_3d:
+                        a.append(ax.plot(np.zeros(3), np.zeros(3), p_type[m])[0])
+                    else:
+                        a.append(ax.plot(np.zeros(2), np.zeros(2), p_type[m])[0])
+                pose.append(a)
+            ax.axis([-1, 1, -1, 1])
+            if is_3d:
+                ax.set_zlim3d(-1, 1)
+            for t in range(T):
+                for m in range(M):
+                    for i, (v1, v2) in enumerate(edge):
+                        x1 = data[0, :2, t, v1, m]
+                        x2 = data[0, :2, t, v2, m]
+                        if (x1.sum() != 0 and x2.sum() != 0) or v1 == 1 or v2 == 1:
+                            pose[m][i].set_xdata(data[0, 0, t, [v1, v2], m])
+                            pose[m][i].set_ydata(data[0, 1, t, [v1, v2], m])
+                            if is_3d:
+                                pose[m][i].set_3d_properties(data[0, 2, t, [v1, v2], m])
+                fig.canvas.draw()
+                # plt.savefig('/home/lshi/Desktop/skeleton_sequence/' + str(t) + '.jpg')
+                plt.pause(0.01)
+
+
+if __name__ == '__main__':
+    import os
+
+    os.environ['DISPLAY'] = 'localhost:10.0'
+    data_path = "../data/ntu/xview/val_data_joint.npy"
+    label_path = "../data/ntu/xview/val_label.pkl"
+    graph = 'graph.ntu_rgb_d.Graph'
+    test(data_path, label_path, vid='S004C001P003R001A032', graph=graph, is_3d=True)
+    # data_path = "../data/kinetics/val_data.npy"
+    # label_path = "../data/kinetics/val_label.pkl"
+    # graph = 'graph.Kinetics'
+    # test(data_path, label_path, vid='UOD7oll3Kqo', graph=graph)
